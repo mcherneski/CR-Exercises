e68fc8a5d7d6396a7ddd3038eccef6335564cd24
Cyberd-Mike <72850479+Cyberd-Mike@users.noreply.github.com>
2021-09-09 14:55:24 -0600

    Renamed sql, changed readme.

--- CSV_Exporter/GamedayExport.py
+++ CSV_Exporter/GamedayExport.py
@@ -42,19 +42,27 @@ a_venue = []
 a_venue_id = []
 
 def GetRequestedDate():
-# Get the date from the user, if they do not specify, today is selected.
-    requestedDate = input("Please enter in a date. Format: YYYY-MM-DD. Leave blank for today. \n  Date: ")
+#   Check if the user specified a date in the command line arguments. If they didn't, ask for the date. 
+    requestedDate = input("\n Please enter in a date. Format: YYYY-MM-DD. Leave blank for today. \n  Date: ")
     if not requestedDate:
-        requestedDate = (date.today()).strftime("%Y-%m-%d")
-    if requestedDate:
+            requestedDate = (date.today()).strftime("%Y-%m-%d")
 #       Verify the user input the date in our requested format. If it doesn't, it shows an error and reruns the GetRequestedDate function.
-        matched = re.match("[1-2][0-9][0-9][0-9]-[0-1][0-9]-[0-3][0-9]", requestedDate)
-        is_match = bool(matched)
-        if is_match:
-            return(requestedDate)
+    if requestedDate:
+        if ValidateDate(requestedDate): 
+            return requestedDate
         else:
-            print("Error: Please format date as YYYY-MM-DD.")
             GetRequestedDate()
+
+def ValidateDate(requestedDate):
+# Function to validate the entered date. Uses regex pattern recognition to figure out if date is in YYYY-MM-DD format. 
+# Returns true or false.
+    matched = re.match("[1-2][0-9][0-9][0-9]-[0-1][0-9]-[0-3][0-9]", requestedDate)
+    is_match = bool(matched)
+    if is_match:
+        return True
+    else:
+        print("\n Error: Please format date as YYYY-MM-DD.")
+        return False
 
 def InvokeApiRequest(requestURL):
 # Function responsible for the API request. Handles timeout exceptions from the requests module. 
@@ -64,21 +72,21 @@ def InvokeApiRequest(requestURL):
 #   Return the contents of the data variable from function for later use. 
     except requests.exceptions.Timeout:
         while "Invalid response...":
-            reply = (input("We are heving issues communicating with the API... \n Try again? [Y/N]")).capitalize().strip()
+            reply = (input("\n We are heving issues communicating with the API... \n Try again? [Y/N]")).capitalize().strip()
             if (reply == 'Y' or reply == 'YES'):
                 InvokeApiRequest(requestURL)
             if (reply == 'N' or reply == 'NO'):
-                print("Error: Could not reach API server.")
+                print("\n Error: Could not reach API server.")
                 time.sleep(5)
                 sys.exit()
     except requests.exceptions.RequestException as e:
 # Handles all other non-timeout related API errors. Allows retry but errors could be due to malformed URI. 
          while "Invalid response...":
-            reply = (input("Error with web request. Try again? [Y/N]")).capitalize().strip()
+            reply = (input("\n Error with web request. Try again? [Y/N]")).capitalize().strip()
             if (reply == 'Y' or reply == 'YES'):
                 ExportGamedayData()
             if (reply == 'N' or reply == 'NO'):
-                print("Error: Could not reach API server.")
+                print("\n Error: Could not reach API server.")
                 time.sleep(5)
                 raise SystemExit(e)
 
@@ -88,7 +96,7 @@ def CreateCsvFile(requestedDate, gameday_DF):
     try:
 #       The lines below use the Pandas dataframe we create in the main function and uses the .to_csv method from Pandas to create the csv file. 
         gameday_DF.to_csv(csv_file, index=False)
-        print("CSV Exported. Please check the working directory for your file.")
+        print("\n CSV Exported. Please check the working directory for your file.")
         sys.exit()
 #       If the file is created, call sys.exit()
     except IOError:
@@ -105,7 +113,13 @@ def CreateCsvFile(requestedDate, gameday_DF):
 def ExportGamedayData():
 #   Main function which calls other functions from above. Called on last line of program.
 #   Get user's requested date and parse to API request URI
-    requestedDate = GetRequestedDate()
+    if (sys.argv[1]):
+        if ValidateDate(sys.argv[1]):
+            requestedDate = sys.argv[1]
+        else:
+            requestedDate = GetRequestedDate()
+    else:
+        requestedDate = GetRequestedDate()
     requestAPIUrl = "http://gd2.mlb.com/components/game/mlb/year_{}/month_{}/day_{}/grid.json".format(str(requestedDate[0:4]), str(requestedDate[5:7]), str(requestedDate[8:10]))
 #   Receive and parse the API response. 
     apiResponse = (InvokeApiRequest(requestAPIUrl)).json()
@@ -113,15 +127,18 @@ def ExportGamedayData():
         gamedayDataJson = apiResponse["data"]["games"]["game"]
 #       Try block handles non-erroneus failures, such as a day which no games were played.
     except:
-        reply = (input("No game data for that date. \n Try another date? [Y/N]")).capitalize().strip()
-#       Capitalize and remove white spaces so the below if statements are less prone to input variance errors. 
-        if (reply == 'Y' or reply == 'YES'):
-            ExportGamedayData()
-#       If there is no data for the specified date, we ask the user if they would like to try another rather than rerunning the program. 
-        elif (reply == 'N' or reply == 'NO'):
-            sys.exit("Could not find data for requested day.")
+        if sys.argv[1]:
+            sys.exit("\nNo game data for that date. Please try a different date.")
         else:
-            print("Please choose Y or N. (Yes or No)")        
+            reply = (input("No game data for that date. \n Try another date? [Y/N]")).capitalize().strip()
+#           Capitalize and remove white spaces so the below if statements are less prone to input variance errors. 
+            if (reply == 'Y' or reply == 'YES'):
+                ExportGamedayData()
+#           If there is no data for the specified date, we ask the user if they would like to try another rather than rerunning the program. 
+            elif (reply == 'N' or reply == 'NO'):
+                sys.exit("Exiting program.")
+            else:
+                print("Please choose Y or N. (Yes or No)")        
 #   Below, run the gamedayDataJson and extract values. Add them to the arrays created at top of script. 
     for game in gamedayDataJson:
         a_away_code.append(game["away_code"])
--- CSV_Exporter/README.txt
+++ CSV_Exporter/README.txt
@@ -1,19 +1,44 @@
-Running GamedayExport.py
+# GamedayExport
 
-Use this utility to export game day data to a CSV file. 
+Gameday Export is a Python script which downloads MLB game data for a specified date, and exports the data via a CSV file. 
+The CSV file will be stored in the same directory as the GamedayExport.py file and will be named for the date which you requested. 
 
-1. Before you run GamedayExport.py, please run the following command to install dependencies. 
+Example: 2016-08-04_GamedayData.csv
+
+## Installation
+
+In order to run GamedayExport.py, you will first need to install Python2 or 3. Once Python is installed, install the GamedayExport dependencies 
+by opening up Terminal (Mac/Linux) or Command Prompt (Windows), navigating to the GamedayExport directory and typing the following command:
 
     pip install -r requirements.txt
 
-2. To run the script, open a terminal (Mac/Linux) or command prompt window (Windows) and type the following command.
+This will install two Python modules, "Pandas" and "Requests". You can manually install these dependencies with the pip install MODULENAME command as well.
 
-    python3 GamedayExport.py
+## Usage
 
-3. The script will ask for a date. Please enter the date in the YYYY-MM-DD format. 
+There are two ways to run the script, with parameters and without. 
 
-4. The CSV file will be saved in the same directory as the script. Your requested date will be the prefix to a _Gameday_Data.csv file. 
+    WITHOUT PARAMETERS
+    Open Terminal (Mac/Linux) or Command Prompt (Windows) and type: 
 
-    Example: 2016-08-04_Gameday_Data.csv
+        python GamedayExport.py 
+    
+    The script will then ask you for a date, please enter in your desired date in the YYYY-MM-DD format. If the date is formatted correctly, the script will ask
+    for a new entry. If there is no game data for that date, the script will ask if you want to try for another date. 
 
-NOTE: If at any time you want to cancel the program's execution, press Ctrl + C on both Windows and Mac/Linux systems. 
+    WITH PARAMETERS
+    Open Terminal (Mac/Linux) or Command Prompt (Windows) and type:
+
+        python GamedayExport.py DATE 
+        
+        NOTE: Replace DATE with your desired date, in YYYY-MM-DD format. 
+    This way, you can quickly specify a target date without the program asking. This is useful if you want to run this script for multiple dates quickly. 
+    Please note if there is no data for that date the script will exit. If the date format is incorrect, the script will ask you for a new date.
+
+    If at any time you get stuck while using this script, press Ctrl + C to end script execution and exit. 
+
+## Contributing
+    N/A
+    
+## License
+    N/A    
